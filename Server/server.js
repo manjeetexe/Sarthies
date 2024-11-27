91e3b551299c036de2a822b9c2c3e01fee9a39e3
manjeetexe <manjeetsharma9257@gmail.com>
2024-11-27 16:28:09 +0530

    cros

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cookieParser());
-app.use(cors({ origin: 'https://sarthies-3.onrender.com/', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.json({ limit: '60mb' }));
 app.use(express.urlencoded({ limit: '60mb', extended: true }));
