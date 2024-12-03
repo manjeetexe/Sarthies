76afe7f3d9bd099ead3827fb1b32bbd4a474b5c6
manjeetexe <manjeetsharma9257@gmail.com>
2024-12-03 19:52:48 +0530

    link

--- Client/src/pages/AnalizeMCQ.jsx
+++ Client/src/pages/AnalizeMCQ.jsx
@@ -169,7 +169,7 @@ const Analysis = () => {
       formData.append('screenshotImage', screenshotBlob, 'screenshot.jpg');
   
       // Send data
-      const response = await fetch('http://localhost:8000/send-analyze-email', {
+      const response = await fetch('https://sarthies-4.onrender.com/send-analyze-email', {
         method: 'POST',
         body: formData, // Use FormData instead of JSON
       });
