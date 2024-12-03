e4f1d9e70222554f086fa6ada895f330e2ec0af9
manjeetexe <manjeetsharma9257@gmail.com>
2024-12-03 20:06:40 +0530

    url

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://sarthies-4.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
+  
   plugins: [react()],
 })
